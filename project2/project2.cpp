923dda8665ab08a7238cbf09841b953e37c06bb6
arifa <arifa@muohio.edu>
2014-10-16 17:16:14 -0400

    final project 2

---
+++ project2/project2.cpp
@@ -0,0 +1,148 @@
+//Project 2//
+
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<string>
+#include<stdlib.h>
+#include<math.h>
+#include<stdexcept>
+using namespace std;
+
+
+string name;//name of the students
+string s;//score of the students
+double score;
+double mean=0.0;
+double sum=0.0;
+double sd;
+double var=0.0;
+
+vector<string>names;
+vector<double>scores;
+
+int nameandgrade();
+
+void meanandsd();
+
+char  gradeofscore(double score);
+
+//Main function//                                                            
+ 
+int main()
+{
+
+  cout<<"Enter name and score or enter 'none' to quit"<<endl;
+  nameandgrade();
+  meanandsd();
+  cout<<"name              "<<"score  "<<setprecision(3)<<"Grade "<<endl;
+  for(int i=0;i<scores.size();i++)
+    {
+      cout<<i+1<<"."<<names.at(i)<<"  "<<scores.at(i)<<"%"<< \
+	"  "<<gradeofscore(scores.at(i))<<endl;
+    }
+  cout<<"Clss average ="<<setprecision(4)<<mean<<"%"<<endl;
+  cout<<"Standerd deviation ="<<setprecision(4)<<sd<<endl;
+
+  return 0;
+
+}
+
+//Taking information about students(Name & Score)//
+int  nameandgrade()
+{
+ Loop:while(1)
+    {
+       cout<<"Enter name:"<<endl;
+       getline(cin,name);
+           if(name=="none")
+            {
+             break;
+             }
+       cin.clear();
+       cin.sync();
+           if(name.empty())
+	     {
+	       cout<<"invalid input,try again"<<endl;
+	       goto Loop;
+	     }
+       cin.clear();
+       cin.sync();
+       names.push_back(name);
+
+       cout<<"Enter score:"<<endl;
+       getline(cin,s);
+       score=atof(s.c_str());
+       try
+       {
+          if(score<0)
+            { 
+             throw out_of_range("Score cannot be negative");
+            }
+	  if(score>100)
+            {
+	     throw out_of_range("Score cannot be greater than 100");
+            }
+
+        scores.push_back(score);
+        }
+ 
+       catch(out_of_range err)
+      {
+	cout<<err.what()<<"\n"<<" try again...."<<endl;
+      cin.clear();
+      cin.sync();
+      } 
+    }
+}
+
+     
+
+//Determining Mean and Standard Deviation//
+
+void meanandsd() 
+{   
+  for (int i=0;i<scores.size();i++)
+    {
+      sum=sum+scores.at(i);
+    }
+  mean=sum/scores.size();
+  for (int i=0;i<scores.size();i++)
+    {
+      var=var+pow((scores.at(i)-mean),2.0);
+    }
+  sd=sqrt(var/scores.size());
+       
+  //Finding the upper limit and lower limit of grades//
+ 
+  cout<<endl;
+  cout<<endl;
+  cout<<"Grading scale"<<endl;
+  cout<<"A above "<<setprecision(3)<<mean+1.5*sd<<"%"<<endl;
+  cout<<"B "<<setprecision(3)<<mean+0.5*sd<<"%-"<<mean+1.5*sd<<"%"<<endl;
+  cout<<"C "<<setprecision(3)<<mean-0.5*sd<<"%-"<<mean+0.5*sd<<"%"<<endl;
+  cout<<"D "<<setprecision(3)<<mean-1.5*sd<<"%-"<<mean-0.5*sd<<"%"<<endl;
+  cout<<"F below "<<setprecision(3)<<mean-1.5*sd<<"%"<<endl;
+}
+
+//Determining grades of individual students//
+
+char  gradeofscore(double score)
+{
+  char  A,B,C,D,F;
+  if(score>mean+1.5*sd) 
+    return 'A';
+  if(score>mean+0.5*sd&&score<mean+1.5*sd)
+    return 'B';
+  if(score>mean-0.5*sd&&score<mean+0.5*sd)
+    return 'C';
+  if(score>mean-1.5*sd&&score<mean-0.5*sd)
+    return 'D';
+  if (score<mean-1.5*sd)
+    return 'F';
+}
+
+
+
+
+
